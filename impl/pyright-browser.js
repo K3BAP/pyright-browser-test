e0f05bc20b75d40571e834527bf8be0d7181d4f7
Fabian Sponholz <f.sponholz00@gmail.com>
2024-04-21 15:04:01 +0200

    establish connection to pyright and successfully initialize the server with usable parameters

--- impl/pyright-browser.js
+++ impl/pyright-browser.js
@@ -5,16 +5,76 @@ import {
     BrowserMessageWriter,
   } from "vscode-jsonrpc/browser";
 
-// Initialize the worker with the JavaScript file
-const worker = new Worker(workerScript);
-// Send the `boot` request to the language server
-// This starts the language server and a nested worker that performs the language analysis.
-worker.postMessage({
-    type: 'browser/boot',
-    mode: 'foreground'
-});
+import {
+    InitializeRequest,
+    LogMessageNotification,
+    PublishDiagnosticsNotification,
+    RegistrationRequest,
+    InitializeParams
+} from 'vscode-languageserver-protocol'
 
-const connection = createMessageConnection(
-    new BrowserMessageReader(worker),
-    new BrowserMessageWriter(worker)
-);
+
+
+async function initialize() {
+    // Initialize the worker with the JavaScript file
+    const worker = new Worker(workerScript);
+    // Send the `boot` request to the language server
+    // This starts the language server and a nested worker that performs the language analysis.
+    worker.postMessage({
+        type: 'browser/boot',
+        mode: 'foreground'
+    });
+
+    const connection = createMessageConnection(
+        new BrowserMessageReader(worker),
+        new BrowserMessageWriter(worker)
+    );
+
+    connection.onNotification(LogMessageNotification.type, (params) =>
+        console.log("[LS]", params.message)
+    );
+
+    connection.onNotification(
+    PublishDiagnosticsNotification.type,
+    (params) => {
+        const diagString = JSON.stringify(params, null, 2);
+        consoleTextArea.value = diagString;
+        console.log(`new diagnostics! \n${diagString}`)
+        }
+    );
+
+    /** @type {InitializeParams} */
+    const initializeParams = {
+      rootUri: null,
+      capabilities: {
+        textDocument: {
+          diagnostic: {
+            dynamicRegistration: false,
+            relatedDocumentSupport: false
+          }
+        }
+      },
+      trace: "off",
+      locale: undefined,
+    };
+
+    connection.listen();
+    const initializeResponse = await connection.sendRequest(InitializeRequest.type, initializeParams);
+
+    console.log(`Server initialized.\n${JSON.stringify(initializeResponse, null, 2)}`)
+}
+
+window.addEventListener('load', async () => {
+    initialize();
+    //checkButton.onclick = onButtonClick;
+  });
+
+
+  /** @type {HTMLTextAreaElement | null} */
+const inputTextArea = document.querySelector('.code-input');
+
+/** @type {HTMLTextAreaElement | null} */
+const consoleTextArea = document.querySelector('.console');
+
+/** @type {HTMLButtonElement | null} */
+const checkButton = document.querySelector('.button');
